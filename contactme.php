4bc5f8492b8de21e6604d312bdac3bfaae5f4877
manuhatho <amateur9525@gmail.com>
2024-10-11 16:04:36 +0100

    c

--- contactme.php
+++ contactme.php
@@ -10,14 +10,14 @@ try {
     $mail->isSMTP();
     $mail->Host = "smtp.mailgun.org"; // Serveur SMTP de Mailgun
     $mail->SMTPAuth = true; // Authentification SMTP activée
-    $mail->Username = "postmaster@sandboxa7edcb20b247449c8012a8862936ec68.mailgun.org"; // SMTP login Mailgun
-    $mail->Password = "8ab38af62a97b17c71ea7bd9458f0be5-826eddfb-4cc5511d";  // Mot de passe Mailgun
+    $mail->Username = "postmaster@sandboxfb5329d28e9a4137bef8345422b35a75.mailgun.org"; // SMTP login Mailgun
+    $mail->Password = "eb18f0b56940b256d0e3322d03a699ee-5dcb5e36-954e4ad4";  // Mot de passe Mailgun
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Sécurité TLS
     $mail->Port = 587; // Port SMTP
 
     // Destinataire et contenu
-    $mail->setFrom('graphicdesignhome00@gmail.com', 'Manu Hatho'); // Mon adresse mail et le nom
-    $mail->addAddress('graphicdesignhome00@gmail.com'); // Adresse destinataire (remplacer par ton adresse)
+    $mail->setFrom('manuella.hatho@gmail.com', 'Manu Hatho'); // Mon adresse mail et le nom
+    $mail->addAddress('manuella.hatho@gmail.com'); // Adresse destinataire (remplacer par ton adresse)
     $mail->isHTML(false);
     $mail->Subject = "Nouveau contact via le formulaire de ton Portfolio";
     $mail->Body = "Full Name: " .
