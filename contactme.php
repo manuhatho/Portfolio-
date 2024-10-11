490c5b02312e3f3325fb592c837b7e2115ea2873
manuhatho <amateur9525@gmail.com>
2024-10-11 17:58:24 +0100

    g

--- contactme.php
+++ contactme.php
@@ -11,14 +11,14 @@ try {
     $mail->isSMTP();
     $mail->Host = "smtp-relay.brevo.com"; // Serveur SMTP correct
     $mail->SMTPAuth = true; // Authentification SMTP activée
-    $mail->Username = "7dc532001@smtp-brevo.com"; // SMTP login Brevo
-    $mail->Password = "Y2bdFAESZctrT0WM";  // Nouveau mot de passe
+    $mail->Username = "7dc532002@smtp-brevo.com"; // SMTP login Brevo
+    $mail->Password = "820EhZAnaGIr5W16";  // Nouveau mot de passe
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Sécurité TLS
     $mail->Port = 587; // Port SMTP
 
     // Destinataire et contenu
-    $mail->setFrom('manuella.hatho@gmail.com', 'Manu Hatho'); // Mon adresse mail et le nom
-    $mail->addAddress('manuella.hatho@gmail.com'); // Adresse destinataire (remplacer par ton adresse)
+    $mail->setFrom('manuhatho@outlook.com', 'Manu Hatho'); // Mon adresse mail et le nom
+    $mail->addAddress('manuhatho@outlook.com'); // Adresse destinataire (remplacer par ton adresse)
     $mail->isHTML(false);
     $mail->Subject = "Nouveau contact via le formulaire de ton Portfolio";
     $mail->Body = "Full Name: " .
