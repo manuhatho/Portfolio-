71d8120882e94af78f63c63a753b3a58d76841bc
manuhatho <amateur9525@gmail.com>
2024-10-12 14:08:53 +0100

    n

--- fullscreen.js
+++ fullscreen.js
@@ -1,17 +1,17 @@
-// Fonction pour activer le plein écran
-function launchFullScreen(element) {
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) { // Firefox
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { // IE/Edge
-      element.msRequestFullscreen();
-    }
-  }
+// // Fonction pour activer le plein écran
+// function launchFullScreen(element) {
+//     if (element.requestFullscreen) {
+//       element.requestFullscreen();
+//     } else if (element.mozRequestFullScreen) { // Firefox
+//       element.mozRequestFullScreen();
+//     } else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
+//       element.webkitRequestFullscreen();
+//     } else if (element.msRequestFullscreen) { // IE/Edge
+//       element.msRequestFullscreen();
+//     }
+//   }
   
-  // Activer le plein écran sur un événement tactile
-  document.addEventListener('touchstart', function() {
-    launchFullScreen(document.documentElement); // Activer le plein écran pour l'élément racine (document)
-  });
+//   // Activer le plein écran sur un événement tactile
+//   document.addEventListener('touchstart', function() {
+//     launchFullScreen(document.documentElement); // Activer le plein écran pour l'élément racine (document)
+//   });
